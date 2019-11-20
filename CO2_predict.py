adee004ed35a74984220d10beb9c9e7d274f9c2d
bs-28 <56816868+bs-28@users.noreply.github.com>
2019-11-20 22:00:45 +1100

    Add files via upload
    Prediction values

---
+++ CO2_predict.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 21:26:36 2019
+
+@author: bhumikasinghal
+"""
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+np.set_printoptions(suppress=True)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+from sklearn.linear_model import LinearRegression
+
+if __name__ == "__main__":
+    print("--------------- CO2 Emission -------------")
+    df_fr = pd.read_csv('CO2.csv', error_bad_lines=False, skiprows=4)
+    df_fr = df_fr.drop(['Country Code','Indicator Name','Indicator Code','Unnamed: 64'], axis=1)
+    df_fr = df_fr.set_index('Country Name')
+    df_fr = df_fr.dropna(how = 'all', axis=0)
+    df_fr = df_fr.dropna(how = 'all', axis=1)
+    df_fr = df_fr[df_fr.isnull().sum(axis=1) <= 20]
+    df_fr = df_fr.ffill(axis=1)
+    df_fr = df_fr.bfill(axis=1)
+    #print(df_fr.shape)
+    df_fr = df_fr.reset_index()
+    FR_Stacked_df = pd.melt(df_fr,id_vars=['Country Name'])
+    FR_Stacked_df = FR_Stacked_df.set_index("Country Name")
+    FR_Stacked_df['variable'] = FR_Stacked_df['variable'].astype(float)
+    #sns.regplot(x="variable", y="value", data=FR_Stacked_df);
+    #plt.show()
+    
+    le = preprocessing.LabelEncoder()
+    FR_Stacked_df = FR_Stacked_df.reset_index()
+    FR_Stacked_df['encoded'] = le.fit_transform(FR_Stacked_df["Country Name"].values)
+    X = FR_Stacked_df[['encoded','variable']]
+    X = np.array(X)
+    y = FR_Stacked_df['value']
+    y = np.array(y)
+    
+    model = KNeighborsRegressor(n_neighbors=3)
+    model.fit(X , y)
+    
+    cont = input("Enter Country Name :")
+    year = int(input("Enter year :"))
+    enc = FR_Stacked_df.encoded[FR_Stacked_df["Country Name"]==cont].unique()[0]
+    predicted = model.predict([[enc, year]]).astype(float)
+    print("Predicted CO2 emission: ",predicted[0],"kt")
+    
+    new_df=FR_Stacked_df[FR_Stacked_df["Country Name"]==cont]
+    sns.lineplot(x="variable" , y="value",data=new_df)
+    plt.show()
---
+++ gdp_predict.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+np.set_printoptions(suppress=True)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+from sklearn.linear_model import LinearRegression
+
+if __name__ == "__main__":
+    print("--------------- GDP-------------")
+    df_gdp = pd.read_csv("gdp.csv",skiprows=3)
+    df_gdp.head()
+    
+    df_gdp = df_gdp.drop("Unnamed: 64",axis=1)
+    df_gdp = df_gdp.drop("2019",axis=1)
+
+    df_gdp.describe(include='all')
+
+    df_gdp = df_gdp.drop(["Indicator Name","Indicator Code"],axis=1)
+
+    GDP_Stacked_df = pd.melt(df_gdp,id_vars=['Country Name','Country Code'])
+
+    #print(GDP_Stacked_df.head())
+
+    GDP_Stacked_df.loc[GDP_Stacked_df['value'].isnull(), 'value'] = GDP_Stacked_df['Country Name'].map(GDP_Stacked_df.groupby('Country Name')['value'].mean())
+
+    GDP_Stacked_df = GDP_Stacked_df.dropna(axis=0)
+    #print(GDP_Stacked_df.head())
+    GDP_Stacked_df=GDP_Stacked_df.set_index("Country Name")
+    
+    GDP_Stacked_df['variable'] = GDP_Stacked_df['variable'].astype(float)
+
+    #sns.regplot(x="variable", y="value", data=GDP_Stacked_df);
+    #plt.show()
+
+    le = preprocessing.LabelEncoder()
+    
+    GDP_Stacked_df= GDP_Stacked_df.reset_index()
+    GDP_Stacked_df.head()
+
+    GDP_Stacked_df['encoded'] = le.fit_transform(GDP_Stacked_df["Country Name"].values)
+
+    X = GDP_Stacked_df[['encoded','variable']]
+    X = np.array(X)
+    
+    y = GDP_Stacked_df['value']
+    y = np.array(y)
+    print(GDP_Stacked_df[GDP_Stacked_df["encoded"]==11])
+    #X_train, X_val, Y_train, Y_val = train_test_split(X_train_val, Y_train_val, test_size=0.15/0.85, random_state=0)
+
+    model = LinearRegression()
+    model.fit(X , y)
+
+    cont = input("Enter Country Name :")
+    year = int(input("Enter year :"))
+    enc = GDP_Stacked_df.encoded[GDP_Stacked_df["Country Name"]==cont].unique()[0]
+    
+
+
+    predicted = model.predict([[enc, year]]).astype(float)
+    print("Predicted GDP: ",predicted[0],"US$")
+
+
+    new_df=GDP_Stacked_df[GDP_Stacked_df["Country Name"]==cont]
+    sns.lineplot(x="variable" , y="value",data=new_df)
+    plt.show()
+    
