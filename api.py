93f9bc024059ff00383c32fbfe5618ca8ab27329
saurabhacharya24 <saurabhacharya123@gmail.com>
2019-11-21 00:09:58 +1100

    Restructured directory, added literacy chart to frontend

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
+import io
 from flask import Flask, request
 from flask_restplus import Resource, Api
 from flask_cors import CORS
+from literacyPrediction import *
+import json
+# import matplotlib as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -38,8 +42,16 @@ class Auth(Resource):
     
         if body['username'] == 'user' and body['password'] == 'password':
             auth.set_auth(True, 'user')
-            print(auth.check_auth())
-            return {"message": "Success"}, 200
+            # print(auth.check_auth())
+
+            plt_line = plotRatePerCountryImputed(29)
+            plt_bar = plotYearBar(2022)
+            
+            return {"bar_x": json.dumps(plt_bar[0].tolist()), 
+                    "bar_y": json.dumps(plt_bar[1].tolist()),
+                    "message": "Success", 
+                    "x": json.dumps(plt_line[0].tolist()), 
+                    "y": json.dumps(plt_line[1].tolist())}, 200
 
         else:
             print(auth.check_auth())
