6c23258881415ee5e132ddf59cb1feb7a02b25e2
saurabhacharya24 <saurabhacharya123@gmail.com>
2019-11-22 01:24:12 +1100

    updated lifeexpec_predict.py to return predicted value and saves figure to 'predicted_images' folder

--- lifeexpec_predict.py
+++ lifeexpec_predict.py
@@ -6,6 +6,8 @@ Created on Thu Nov 14 20:44:22 2019
 @author: bhumikasinghal
 """
 
+import matplotlib
+matplotlib.use('Agg') # Need to add this otherwise server closes unexpectedly
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -18,9 +20,9 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 
-if __name__ == "__main__":
-    print("--------------- Life Expectancy -------------")      
-    df_le = pd.read_csv('life_expectancy.csv', error_bad_lines=False, skiprows=4)
+def predicted_life_expec(cont, year):
+    # print("--------------- Life Expectancy -------------")      
+    df_le = pd.read_csv('Backend/Datasets/life_expectancy.csv', error_bad_lines=False, skiprows=4)
     df_le = df_le.drop(['Country Code','Indicator Name','Indicator Code','2018','2019','Unnamed: 64'], axis=1)
     df_le = df_le.set_index('Country Name')
     df_le = df_le.dropna(how = 'all', axis=0)
@@ -46,13 +48,29 @@ if __name__ == "__main__":
     model = LinearRegression()
     model.fit(X , y)
 
-    cont = input("Enter Country Name :")
-    year = int(input("Enter year :"))
+    # cont = input("Enter Country Name :")
+    # year = int(input("Enter year :"))
     enc = LE_Stacked_df.encoded[LE_Stacked_df["Country Name"]==cont].unique()[0]
     predicted = model.predict([[enc, year]]).astype(float)
-    print("Predicted Life Expectancy: ",predicted[0])
-    
+    # print("Predicted Life Expectancy: ",predicted[0])
+
+
+    year = int(year)
+
     new_df=LE_Stacked_df[LE_Stacked_df["Country Name"]==cont]
+
+    predicted_df = pd.DataFrame({"Country Name": [cont],
+                                 "variable": [year],
+                                 "value": [predicted[0]],
+                                 "encoded": 9})
+    new_df = new_df.append(predicted_df, ignore_index=True)
+
+    # print(new_df.to_string())
     sns.lineplot(x="variable" , y="value",data=new_df)
-    plt.show()
+    plt.xlabel("Years")
+    plt.ylabel("Life Expectancy")
+    plt.title("Life Expectancy for " + cont)
+    plt.savefig("predicted_images/life_expec.png")
+    plt.close()
+    return predicted[0]
     
